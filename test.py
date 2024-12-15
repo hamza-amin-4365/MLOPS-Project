170f32733d330c1871a936fba15e31ad1437c22f
Hamza Amin <mh4070685@gmail.com>
2024-12-15 21:59:17 +0500

    docker and github actions

--- test.py
+++
@@ -1,7 +0,0 @@
-import dagshub
-dagshub.init(repo_owner='mh4070685', repo_name='MLOPS-Project', mlflow=True)
-
-import mlflow
-with mlflow.start_run():
-  mlflow.log_param('parameter name', 'value')
-  mlflow.log_metric('metric name', 1)
