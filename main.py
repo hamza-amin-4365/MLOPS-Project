37875d8406daf79058812494e97abed376c593ca
Hamza Amin <mh4070685@gmail.com>
2024-12-15 20:15:39 +0500

    model training pipeline

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.mlProject import logger
 from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionStage
 from src.mlProject.pipeline.stage_02_data_validation import DataValidationStage
 from src.mlProject.pipeline.stage_03_data_transfromation import DataTransformationStage
-
+from src.mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion"
@@ -33,4 +33,14 @@ try:
         logger.info(f">>>>>>>>>>Completed {STAGE_NAME} Stage<<<<<<<<<<")
 except Exception as e:
         logger.error(f"Stage: {STAGE_NAME} failed with error: {str(e)}")
+        raise e
+
+STAGE_NAME = "Model Training"
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
         raise e
